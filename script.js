1b72529301ea6978f116e96098c17a43d25fbf45
quik214 <68325502+quik214@users.noreply.github.com>
2023-06-13 14:03:02 +0800

    Replaced with updated files

--- script.js
+++ script.js
@@ -31,8 +31,31 @@ btnOpenNavEl.addEventListener("click", function () {
 // Toggle between project types
 const residentialToggle = document.getElementById("residential");
 const commercialToggle = document.getElementById("commercial");
-const tapestryImg = document.getElementById("tapestry");
-const flamingoImg = document.getElementById("flamingo");
+const retrofitToggle = document.getElementById("retrofit");
+const institutionalToggle = document.getElementById("institutional");
+const infrastructureToggle = document.getElementById("infrastructure");
+const industrialToggle = document.getElementById("industrial");
+const fittingToggle = document.getElementById("fitting");
+
+// Place all toggles inside toggles as an array
+let toggles = Array.from(document.querySelectorAll(".project-btn"));
+
+// handleClick function, executes onclick
+// for each of the element in toggles, remove the 'active' class
+// for the currentTarget, which is the element which started the event,
+// add 'active' class to it
+const handleClick = (e) => {
+  e.preventDefault();
+  toggles.forEach((node) => {
+    node.classList.remove("active");
+  });
+  e.currentTarget.classList.add("active");
+};
+
+// starts the function execution above onclick of a toggle element
+toggles.forEach((node) => {
+  node.addEventListener("click", handleClick);
+});
 
 // commercialToggle.onclick = function () {
 //   if (tapestryImg.style.display !== "none") {
@@ -42,16 +65,19 @@ const flamingoImg = document.getElementById("flamingo");
 //   }
 // };
 
-residentialToggle.onclick = function () {
-  tapestryImg.style.display = "block";
-  flamingoImg.style.display = "none";
-};
+// const tapestryImg = document.getElementById("tapestry");
+// const flamingoImg = document.getElementById("flamingo");
 
-commercialToggle.onclick = function () {
-  tapestryImg.style.display = "none";
-  flamingoImg.style.display = "block";
+// residentialToggle.onclick = function () {
+//   tapestryImg.style.display = "block";
+//   flamingoImg.style.display = "none";
+// };
 
-  // if (flamingoImg.style.display == "none") {
-  //   flamingoImg.style.display = "block";
-  // }
-};
+// commercialToggle.onclick = function () {
+//   tapestryImg.style.display = "none";
+//   flamingoImg.style.display = "block";
+// };
+
+// if (flamingoImg.style.display == "none") {
+//   flamingoImg.style.display = "block";
+// }
