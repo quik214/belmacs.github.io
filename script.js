e845363b1dfe43d9b56827cd6cb81aba26f77e77
quik214 <68325502+quik214@users.noreply.github.com>
2023-06-14 12:45:48 +0800

    Replaced files

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ btnOpenNavEl.addEventListener("click", function () {
   topEl.classList.toggle("nav-open");
 });
 
-// Toggle between project types
+////////// Toggle between project types ////////////////
 
 // Below code to toggle bottom border on/off
 // Place all toggles inside toggles as an array
@@ -51,112 +51,37 @@ toggles.forEach((node) => {
   node.addEventListener("click", handleClick);
 });
 
-const residentialToggle = document.getElementById("residential-btn");
-const commercialToggle = document.getElementById("commercial-btn");
-const retrofitToggle = document.getElementById("retrofit-btn");
-const institutionalToggle = document.getElementById("institutional-btn");
-const infrastructureToggle = document.getElementById("infrastructure-btn");
-const industrialToggle = document.getElementById("industrial-btn");
-const fittingToggle = document.getElementById("fitting-btn");
+///////// To hide and show respective divs //////////
+
+// Get the parent container element
+const projectNav = document.querySelector(".project-nav");
 
 // Class that all categories have
 let category = Array.from(document.querySelectorAll(".category"));
 
-let commercial = document.getElementById("commercial");
-let residential = document.getElementById("residential");
-let retrofit = document.getElementById("retrofit");
-let institutional = document.getElementById("institutional");
-let infrastructure = document.getElementById("infrastructure");
-let industrial = document.getElementById("industrial");
-let fitting = document.getElementById("fitting");
+// Add a click event listener to the parent container
+projectNav.addEventListener("click", function (e) {
+  // e being the clicked element
+  const target = e.target;
 
-// Onclick residential
-residentialToggle.onclick = function () {
-  category.forEach((node) => {
-    node.classList.remove("display");
-  });
-  residential.classList.add("display");
-  return false;
-};
+  // Check if the clicked element is a button
+  if (target.matches(".project-btn")) {
+    // Prevent default behaviour
+    e.preventDefault();
 
-// Onclick commercial
-commercialToggle.onclick = function () {
-  category.forEach((node) => {
-    node.classList.remove("display");
-  });
-  commercial.classList.add("display");
-  return false;
-};
+    // Assign the clicked element's ID to toggleID
+    const toggleId = target.id;
+    // Replace the -btn with blank, resulting in the ID of respective container
+    const contentId = toggleId.replace("-btn", "");
+    // Assign the element with the ID to contentElement
+    const contentElement = document.getElementById(contentId);
 
-// Onclick retrofit
-retrofitToggle.onclick = function () {
-  category.forEach((node) => {
-    node.classList.remove("display");
-  });
-  retrofit.classList.add("display");
-};
+    // Remove "display" class from all elements with the class 'category'
+    category.forEach((node) => {
+      node.classList.remove("display");
+    });
 
-// Onclick institutional
-institutionalToggle.onclick = function () {
-  category.forEach((node) => {
-    node.classList.remove("display");
-  });
-  institutional.classList.add("display");
-};
-
-// Onclick infrastructure
-infrastructureToggle.onclick = function () {
-  category.forEach((node) => {
-    node.classList.remove("display");
-  });
-  infrastructure.classList.add("display");
-};
-
-// Onclick industrial
-industrialToggle.onclick = function () {
-  category.forEach((node) => {
-    node.classList.remove("display");
-  });
-  industrial.classList.add("display");
-};
-
-// Onclick fitting
-fittingToggle.onclick = function () {
-  category.forEach((node) => {
-    node.classList.remove("display");
-  });
-  fitting.classList.add("display");
-};
-
-// const handleToggle = (e) => {
-//   e.preventDefault();
-//   categoryCards.forEach((node) => {
-//     node.classList.remove("display");
-//   });
-//   e.current;
-// };
-
-// commercialToggle.onclick = function () {
-//   if (tapestryImg.style.display !== "none") {
-//     tapestryImg.style.display = "none";
-//   } else {
-//     tapestryImg.style.display = "block";
-//   }
-// };
-
-// const tapestryImg = document.getElementById("tapestry");
-// const flamingoImg = document.getElementById("flamingo");
-
-// residentialToggle.onclick = function () {
-//   tapestryImg.style.display = "block";
-//   flamingoImg.style.display = "none";
-// };
-
-// commercialToggle.onclick = function () {
-//   tapestryImg.style.display = "none";
-//   flamingoImg.style.display = "block";
-// };
-
-// if (flamingoImg.style.display == "none") {
-//   flamingoImg.style.display = "block";
-// }
+    // Add "display" class to the corresponding content element
+    contentElement.classList.add("display");
+  }
+});
